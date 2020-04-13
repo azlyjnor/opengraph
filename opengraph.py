bfdcfc5ed8ed9d4340a371fa5d92086d792ffb90
Azly J. Nor <aj@blackwilder.com>
2020-04-13 13:37:21 +0800

    first commit

---
+++ opengraph.py
@@ -0,0 +1,13 @@
+from newspaper import Article
+
+#testing out newspaper to see if values work
+url = [input-url]
+
+article = Article(url)
+article.download()
+article.parse()
+article.publish_date 
+article.authors
+article.text
+
+print (article.text)
